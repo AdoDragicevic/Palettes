e3cc9fdce67ae1b2c7fdb981cdc7b1dfe8638bcb
Ado Dragicevic <adodragicevic@gmail.com>
2021-08-29 09:38:40 +0200

    Make palette list responsive

--- src/styles/sizes.js
+++ src/styles/sizes.js
@@ -3,7 +3,8 @@ function sizes(size) {
         xs: "575.98px",
         sm: "767.98px",
         md: "991.98px",
-        lg: "1199.98px"
+        lg: "1199.98px",
+        xl: "1600px"
     };
     return `@media screen and (max-width: ${sizes[size]})`;
 };
